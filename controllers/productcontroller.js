b9e2d540d9c4f52bf8265e709d7eeafde718e0e4
poojithamandavalli <105728403+poojithamandavalli@users.noreply.github.com>
2024-01-25 21:00:42 +0530

    Update productcontroller.js for publickey2

--- controllers/productcontroller.js
+++ controllers/productcontroller.js
@@ -12,7 +12,7 @@ const gateway = new braintree.BraintreeGateway({
     environment: braintree.Environment.Sandbox,
     merchantId: process.env.BRAINTREE_MERCHANT_ID,
     // publicKey: process.env.BRAINTREE_PUBLIC_KEY,
-    publicKey:5g8r9wnbt8gmfjzr,
+    publicKey: '5g8r9wnbt8gmfjzr',
     privateKey: process.env.BRAINTREE_PRIVATE_KEY,
   });
 
